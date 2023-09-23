7a39322adc3c764e3690077ac20cc04cdc074dad
naatox <renato.morales@alumnos.ucn.cl>
2023-09-23 14:18:08 -0300

    first commit

---
+++ resources/views/register.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>RENA CACHUITO</h1>
+    <h2 class="mt-2 text-red-5000 text-2x1 uppercase front-bold text-center">RENA CACHUO</h2>
+    <img src= "https://img.freepik.com/vector-premium/ilustracion-dibujos-animados-vaca_29937-10306.jpg?w=2000" />
+
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>RENA CACHUO</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>Buendía</h1>
+    <a href="{{route('register')}}">Click aqui para ver la magia</a>
+</body>
+</html>
